78e6282b09211c97765cb6e91aa704ab1496c3fd
Notebook <pavel1182@yandex.ru>
2023-03-04 15:21:30 +0300

    Fix bug with form.hidden_tag

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, url_for
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +8,7 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
