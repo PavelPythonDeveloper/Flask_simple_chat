4de4fb29fdb4c88b180214af155b3f34de8d2cb5
Notebook <pavel1182@yandex.ru>
2023-03-10 22:20:46 +0300

    add phone field in registration form

--- app/forms.py
+++ app/forms.py
@@ -13,6 +13,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
--- app/models.py
+++ app/models.py
@@ -5,9 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    phone = db.Column(db.String)
 
     def __repr__(self):
-        return f"User(id={self.id}, username={self.username})"
-
-
+        return f'User(id={self.id}, username={self.username})'
