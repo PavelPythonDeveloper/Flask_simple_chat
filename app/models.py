5c9aa050dc42e386466c1d170148706ced6e55b4
Notebook <pavel1182@yandex.ru>
2023-03-11 13:49:27 +0300

    add class Message

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -9,3 +10,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User(id={self.id}, username={self.username})'
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String())
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Message(id={self.id}, " \
+               f"sender_id={self.sender_id}, " \
+               f"recipient_id={self.recipient_id}, " \
+               f"body={self.body}, " \
+               f"timestamp={self.timestamp})"
