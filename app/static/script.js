595d9ddb35d6eb7a12c89336dd1a980b693c50dc
Notebook <pavel1182@yandex.ru>
2023-03-28 13:43:15 +0300

    Add function responseSort

--- app/static/script.js
+++ app/static/script.js
@@ -1,44 +1,58 @@
-function refreshMessages() {
-                        document.querySelectorAll(".message").forEach(el => el.remove());
-                        let requestMessage = new XMLHttpRequest();
-                        var params = 'chat_Id=' + encodeURIComponent(selectedChatId);
-                        requestMessage.open("GET", "/_get_chat_messages?" + params, true);
-                        requestMessage.send();
-                        requestMessage.onload = function()
-                        {
-                            let response = JSON.parse(requestMessage.response);
-                            console.log(response);
-                            for (let key in response)
-                            {
-
-                                let divMessage = document.createElement('div');
-                                divMessage.className = "message";
-                                text = response[key].body;
-                                console.log(text);
-                                divMessage.innerHTML = text;
-                                document.querySelector('#message-holder').append(divMessage);
-                            }
-                        }
+function responseSort(res) {
+    let array = [];
+    for (el in res)
+        {
+            array.push(res[el]);
+        }
+    array.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+    console.log(array);
+    return array;
 }
+
+function refreshMessages()
+{
+    document.querySelectorAll(".message").forEach(el => el.remove());
+    let requestMessage = new XMLHttpRequest();
+    var params = 'chat_Id=' + encodeURIComponent(selectedChatId);
+    requestMessage.open("GET", "/_get_chat_messages?" + params, true);
+    requestMessage.send();
+    requestMessage.onload = function()
+        {
+            let response = JSON.parse(requestMessage.response);
+            console.log(response);
+            let responseArray = responseSort(response);
+
+            for (let i = 0; i < responseArray.length; i++)
+                {
+                    let divMessage = document.createElement('div');
+                    divMessage.className = "message";
+                    divMessage.innerHTML = responseArray[i].body;
+                    document.querySelector('#message-holder').append(divMessage);
+                }
+        }
+}
+
+
 function setMessageCounter(count){
     let messageCounter = document.querySelector("#message-counter");
     messageCounter.innerHTML = count;
 };
 
-function sendButtonPressed() {
-    console.log('Button pressed!');
-    body = document.getElementById('send-message').value
-    let req = new XMLHttpRequest();
-    var params = 'body=' + encodeURIComponent(body) + '&' + 'chat_Id=' + encodeURIComponent(selectedChatId);
-    req.open("POST", "/send_message?" + params, true);
-    req.send();
-    req.onload = refreshMessages;
+
+function sendButtonPressed()
+    {
+        console.log('Button pressed!');
+        body = document.getElementById('send-message').value
+        let req = new XMLHttpRequest();
+        var params = 'body=' + encodeURIComponent(body) + '&' + 'chat_Id=' + encodeURIComponent(selectedChatId);
+        req.open("POST", "/send_message?" + params, true);
+        req.send();
+        req.onload = refreshMessages;
+    }
 
 
-}
 function get_chats()
 {
-
     let request = new XMLHttpRequest();
     var params = 'userId=' + encodeURIComponent(currentUserId);
     request.open("GET", "/_get_chats?" + params, true);
@@ -82,15 +96,15 @@ function get_chats()
                 setMessageCounter(6)
     }
 
-}
-function ready() {
-    let selectedChatId = null;
-const b = document.querySelector('.button');
-b.addEventListener('click', sendButtonPressed);
-get_chats();
 
-let timerId = setInterval(() => get_chats(), 4000);
 }
-
+function ready()
+    {
+        let selectedChatId = null;
+        const b = document.querySelector('.button');
+        b.addEventListener('click', sendButtonPressed);
+        get_chats();
+        let timerId = setInterval(() => get_chats(), 4000);
+    }
 
 document.addEventListener("DOMContentLoaded", ready);
